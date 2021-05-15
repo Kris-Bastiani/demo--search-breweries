8e5b2df569b05a180f57a5ed7270a873056afee8
Kris Bastiani <kris.bastiani@gmail.com>
2021-05-15 12:58:51 +1000

    Add spinner when in progress

--- src/components/Organisms/DemoView.vue
+++ src/components/Organisms/DemoView.vue
@@ -3,11 +3,15 @@
 		<demo-header>
 			<demo-search-input />
 		</demo-header>
+		<div class="demo__view__body">
+			<div v-show="sharedState.isPending" class="demo__spinner" />
+		</div>
 		<demo-footer author="Kris Bastiani" />
 	</main>
 </template>
 
 <script>
+import store from '../../store'
 import DemoHeader from '../Atoms/DemoHeader.vue'
 import DemoFooter from '../Atoms/DemoFooter.vue'
 import DemoSearchInput from '../Atoms/DemoSearchInput.vue'
@@ -18,7 +22,8 @@ export default {
 		DemoHeader,
 		DemoFooter,
 		DemoSearchInput
-	}
+	},
+	data: () => ({ sharedState: store.state })
 }
 </script>
 
@@ -34,5 +39,16 @@ export default {
 	background: $clr__mono_l;
 
 	&__body { padding: 1rem $pad__x; }
+}
+
+.demo__spinner {
+	@include anim_spin;
+	display: block;
+	margin: 0 auto;
+	border: 2px solid $clr__primary;
+	border-top-color: transparent;
+	border-radius: 50%;
+	width: 1rem;
+	height: 1rem;
 }
 </style>
